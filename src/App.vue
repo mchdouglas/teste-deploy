acc2899c5320d8fc859547c036496890535ca621
Michael Douglas <michaeldouglas02@gmail.com>
2020-02-09 15:39:17 -0400

    Fix #1

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      :dark="false"
+      :touchless="true"
+      color="rgb(238, 238, 238)"
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list
+        dense
+        flat
+      >
+        <v-list-item
+          v-for="item in $router.options.routes"
+          :key="item.text"
+          :to="item.path"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.name }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="teal"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">Hifuzion Financeiro</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon  >
+        <v-icon>mdi-account-box</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
---
+++ src/components/dashboard/AgendaPagamentos.vue
@@ -0,0 +1,52 @@
+<template>
+  <DxScheduler
+    :data-source="$store.state.agendaPagamentos"
+    :current-date="currentDate"
+    :views="views"
+    :height="400"
+    :width="800"
+    :start-day-hour="9"
+    current-view="agenda"
+    >
+
+    <DxResource
+      :data-source="resourcesData"
+      field-expr="tipo"
+      label="Tipo"
+    />
+
+  </DxScheduler>
+
+</template>
+<script>
+
+import { DxScheduler, DxResource } from 'devextreme-vue/scheduler'
+
+export default {
+  components: {
+    DxScheduler,
+    DxResource
+  },
+  mounted () {
+    this.$store.dispatch('loadAgendaPagamentos')
+  },
+  data () {
+    return {
+      views: ['agenda', 'week'],
+      currentDate: new Date(2019, 11, 29),
+      resourcesData: [
+        {
+          text: 'Pagamentos',
+          id: 1,
+          color: 'teal'
+        },
+        {
+          text: 'Vencimentos',
+          id: 2,
+          color: 'tomato'
+        }
+      ]
+    }
+  }
+}
+</script>
